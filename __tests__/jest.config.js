ac7b350f13977805abaf00486030116b56feef3e
rstar2 <rstar2@abv.bg>
2017-12-19 13:53:19 +0200

    sadasdsa

--- __tests__/jest.config.js
+++ __tests__/jest.config.js
@@ -1,9 +1,5 @@
 module.exports = {
     'verbose': true,
-    'moduleDirectories': [
-        'src/js',
-        'node_modules'
-    ],
     'testPathIgnorePatterns': ['<rootDir>/e2e/'],
     'testMatch': ['**/?(*.)(spec|test).js'],
     'setupFiles': ['./jest.setup.js']
